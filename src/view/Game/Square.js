dc140648680708d278565aa7285e127407be42ba
Georgiy Starkov <gstarkov658@gmail.com>
2018-02-07 04:50:42 +0300

    Добавит стиль для игры

--- src/view/Game/Square.js
+++ src/view/Game/Square.js
@@ -2,8 +2,22 @@ import React from 'react';
 import './Square.css';
 
 export default function Square(props) {
+	if (props.value === 'X') {
+		return (
+			<div class="cell">
+				<div class="cross hidden"></div>
+			</div>
+		);
+	}
+	if (props.value === 'O') {
+		return (
+			<div class="cell">
+				<div class="circle hidden"></div>
+			</div>
+		);
+	}
 	return (
-		<button className="square" onClick={props.onClick}>
+		<button className="cell" onClick={props.onClick}>
 			{props.value}
 		</button>
 	)
